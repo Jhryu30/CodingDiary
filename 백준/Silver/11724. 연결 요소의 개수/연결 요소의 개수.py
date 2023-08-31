54431a2b5f2cceb82659e4ad255e8a033a60cca4
Jhryu30 <100555769+Jhryu30@users.noreply.github.com>
2023-08-31 14:23:24 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 688 ms, Memory: 65084 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,21 +1,28 @@
 import sys
-sys.setrecursionlimit(10**5)
-N, M = map(int, sys.stdin.readline().rstrip().split())
+from collections import deque
+
+N, M = map(int, sys.stdin.readline().split())
 graph = [[] for _ in range(N+1)]
 for _ in range(M):
-  i,j = map(int, sys.stdin.readline().rstrip().split())
-  graph[i].append(j); graph[j].append(i)
+  u, v = map(int, sys.stdin.readline().split())
+  graph[u].append(v); graph[v].append(u)
 visited = [False for _ in range(N+1)]
 
-def dfs(graph, v, visited):
-  visited[v] = True
-  for new_v in graph[v]:
-    if not visited[new_v]:
-      dfs(graph, new_v, visited)
+def bfs(graph, v, visited):
+  queue = deque()
+  queue.append(v)
+
+  while queue:
+    v = queue.popleft()
+    visited[v] = True
+    for new_v in graph[v]:
+      if not visited[new_v]:
+        visited[new_v] = True
+        queue.append(new_v)
 
 cnt = 0
 for node in range(1,N+1):
   if not visited[node]:
-    dfs(graph, node, visited)
+    bfs(graph, node, visited)
     cnt += 1
 print(cnt)
