28bd201e386e9b97826bcd32ff5186a7034bdb92
Jhryu30 <100555769+Jhryu30@users.noreply.github.com>
2023-09-04 11:17:29 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 68.28 ms, Memory: 26.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+
+def solution(participant, completion):
+    answer = ''
+    participant.sort(); completion.sort()
+    
+    for idx in range(len(completion)):
+        if participant[idx] != completion[idx]:
+            answer = participant[idx] 
+            break
+    if answer == '':
+        answer = participant[-1]
+    return answer
