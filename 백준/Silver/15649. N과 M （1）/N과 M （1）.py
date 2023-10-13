66e76c815eebe1e0e35bcdeb493083baed5d1490
Jhryu30 <100555769+Jhryu30@users.noreply.github.com>
2023-10-13 14:36:54 +0900

    [Silver III] Title: N과 M (1), Time: 152 ms, Memory: 34772 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,19 @@
+N,M = map(int, input().split())
+result_lst = []
+
+def backtracking(result):
+  global result_lst
+  
+  if len(result)==M:
+    result_lst.append(' '.join(list(map(str,result))))
+    return
+
+  for i in range(1,N+1):
+    if i not in result:
+      result.append(i)
+      backtracking(result)
+      result.pop()
+
+backtracking([])
+result_lst.sort()
+print('\n'.join(result_lst))
