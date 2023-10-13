ba891a23575606f07559ba73ef9f7fdc8b103708
Jhryu30 <100555769+Jhryu30@users.noreply.github.com>
2023-10-13 20:08:42 +0900

    [Silver III] Title: 01타일, Time: 356 ms, Memory: 70088 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+dp = [0 for _ in range(N+3)]
+dp[1]=1; dp[2]=2
+
+if N==1 or N==2:
+    print(dp[N])
+else:
+    for i in range(3,N+1):
+        dp[i] = (dp[i-2] + dp[i-1])%15746
+    print(dp[N])
