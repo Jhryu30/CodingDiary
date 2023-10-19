8828e1eff4bd69f28fac4961aeb65fd1c9b9010b
Jhryu30 <100555769+Jhryu30@users.noreply.github.com>
2023-10-19 15:23:59 +0900

    [Bronze II] Title: 대표값2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.py
+++ 백준/Bronze/2587. 대표값2/대표값2.py
@@ -1,7 +1,3 @@
-lst = []
-for _ in range(5):
-  n = int(input(''))
-  lst.append(n)
-lst = sorted(lst)
-print(int(sum(lst)/5))
+lst = [int(input()) for _ in range(5)]; lst.sort()
+print(sum(lst)//5)
 print(lst[2])
