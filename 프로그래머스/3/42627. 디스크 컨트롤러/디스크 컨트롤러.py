e1a1cd35b68141b6530c416bb485129f15ac31c8
Jhryu30 <100555769+Jhryu30@users.noreply.github.com>
2023-10-21 21:42:28 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,26 +1,18 @@
-import heapq
+from collections import deque
 
 def solution(jobs):
     answer = 0
-    N = len(jobs)
     
-    start, t, done = -1,0,0
-    # start:이전 작업 시작 | t:현재 시점 | done:완료된 작업 수
-    heap = []
+    jobs.sort(key=lambda x:[x[1],x[0]])
+    jobs = deque(jobs)
     
-    while done<N:
-        # 현재 시점에 처리 가능한 일들을 heap에 추가
-        for j in jobs:
-            if start<j[0] and j[0]<=t:
-                heapq.heappush(heap, [j[1],j[0]]) #[소요시간,요청시간]
-        if heap:
-            # 지금 처리할 만한 일(=작업 소요시간 가장 적은 일)
-            j = heapq.heappop(heap)
-            start = t
-            t += j[0] # 작업 진행(시간 소모)
-            answer += t-j[1]
-            done += 1
+    t=0
+    while jobs:
+        job = jobs.pop()
+        if job[0]<=t:
+            t+=job[0]
         else:
-            t += 1
-    
-    return answer//N
+            jobs.append(job)
+            jobs.sort()
+            
+    return answer
