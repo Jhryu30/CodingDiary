24089517a844324cb4d8087426620176fef73779
Jhryu30 <100555769+Jhryu30@users.noreply.github.com>
2023-10-25 14:01:04 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,40 +1,25 @@
-from collections import defaultdict, deque
-
 def solution(tickets):
-    answer = ['ICN']
+    answer = []
     N = len(tickets)
-    ticket_dict = defaultdict(list)
-    dest_dict = defaultdict(list)
+    tickets.sort(key=lambda x:x[1])
     
-    for i,(t0,t1) in enumerate(tickets):
-        ticket_dict[t0].append([t1,i])
-        ticket_dict[t1]
-        dest_dict[t1].append(t0); dest_dict[t0]
-    final_destination = 0
-    for t in ticket_dict.keys():
-        ticket_dict[t].sort()
-        if not ticket_dict[t]:
-            final_destination = t
-    
-    def dfs(graph,v,i,visited):
-        nonlocal final_destination
+    def dfs(v,visited,path):
+        nonlocal tickets
+        if len(path)==N+1:
+            # answer.append(path)
+            answer.extend(path)
+            return 
         
-        for new_v,new_i in graph[v]:
-            if not visited[new_i]:
-                if new_v==final_destination and not visited[i]==N-1:
-                    print(new_v,new_i)
-                    pass
-                else:
-                    visited[new_i] = visited[i]+1
-                    dfs(graph,new_v,new_i,visited)
-                    pass
-    
-    v='ICN'; i=ticket_dict['ICN'][0][1]
+        for i,(dep,arr) in enumerate(tickets):
+            if dep == v and not visited[i]:
+                visited[i] = 1
+                dfs(arr, visited,path+[arr])
+                visited[i] = 0
+            if len(answer):
+                break
     visited = [0 for _ in range(N)]
-    dfs(ticket_dict, v, i , visited)
+    dfs('ICN', visited,['ICN'])
+    # answer.sort()
     
-    answer_idx = sorted(range(N), key=lambda x:visited[x])
-    answer += [tickets[i][1] for i in answer_idx]
-    print(visited)
-    print([tickets[i] for i in answer_idx])
+    # return answer[0]
     return answer
