cc255d7acea87316c375412778d08cb1e93eec1a
Jhryu30 <100555769+Jhryu30@users.noreply.github.com>
2024-03-11 17:07:29 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,40 +1,35 @@
-from collections import deque
+from collections import defaultdict, deque
 
 def solution(begin, target, words):
     answer = 0
+    words.append(begin)
+    N,n = len(words), len(words[0])
     
-    if target not in words:
-        return answer
-    
-    words.remove(target)
-    words = [begin]+words+[target]
-    
-    global N
-    N = len(words); word_len = len(begin)
-    graph = [[] for _ in range(N)]
-    for i in range(N):
-        for j in range(i,N):
-            i_word = words[i]; j_word = words[j]
-            if sum([i_word[k]!=j_word[k] for k in range(word_len)])==1:
-                graph[i].append(j); graph[j].append(i)
-          
-    visited = [0 for _ in range(N)]
-    answer = bfs(graph, 0, visited)
-    return answer
-
-
-def bfs(graph, v, visited):
-    queue = deque()
-    queue.append(v)
-    visited[v] = 1
-    
-    while queue:
-        v = queue.popleft()
-        for new_v in graph[v]:
-            if new_v==N-1:
-                return visited[v]
-            if not visited[new_v]:
-                queue.append(new_v)
-                visited[new_v] = visited[v]+1
-    return 0
+    graph = defaultdict(list)
+    visited = {k:0 for k in words}
+    for i,a in enumerate(words[:-1]):
+        for j,b in enumerate(words[i+1:]):
+            cnt = sum(a[idx]!=b[idx] for idx in range(n))
+            if cnt==1:
+                graph[a].append(b); graph[b].append(a)
                 
+    def bfs(visited):
+        nonlocal begin, target, graph
+        queue = deque()
+        queue.append(begin)
+        visited[begin] = 1
+        
+        while queue:
+            v = queue.popleft()
+            for new_v in graph[v]:
+                if new_v == target:
+                    print(v,target)
+                    return visited[v]
+                if not visited[new_v]:
+                    visited[new_v] = visited[v]+1
+                    queue.append(new_v)
+        return 0
+                    
+    answer = bfs(visited)
+    
+    return answer
