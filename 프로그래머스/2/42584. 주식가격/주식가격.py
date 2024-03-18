2afaecd994528f7b51c1379d43b5f5fca66ddcc7
Jhryu30 <100555769+Jhryu30@users.noreply.github.com>
2024-03-18 19:28:39 +0900

    [level 2] Title: 주식가격, Time: 15.28 ms, Memory: 17.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,19 +1,15 @@
 def solution(prices):
-    answer = [len(prices)-i-1 for i in range(len(prices))]
+    N = len(prices)
+    answer = [0 for _ in range(N)]
     
-    # # method.1
-    # for i,p_i in enumerate(prices[:-1]):
-    #     for j,p_j in enumerate(prices[i+1:]):
-    #         if p_i>p_j:
-    #             answer[i]=j+1
-    #             break
-                
-    # method.2
-    previous = []
-    for i,p_i in enumerate(prices):
-        while previous and p_i<previous[-1][1]:
-            j = previous.pop()[0]
-            answer[j]=i-j
-        previous.append([i,p_i])
+    stack = []
+    for i,v in enumerate(prices):
+        while stack and prices[stack[-1]]>v:
+            day = stack.pop()
+            answer[day] = i-day
+        stack.append(i)
     
+    while stack:
+        day = stack.pop()
+        answer[day] = N-day-1
     return answer
