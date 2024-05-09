bdd1cec3aee3fec54d1511fed30e16ba57cb9209
Jhryu30 <100555769+Jhryu30@users.noreply.github.com>
2024-05-09 20:58:49 +0900

    [level 2] Title: N-Queen, Time: 599.63 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -5,13 +5,19 @@ def solution(N):
             answer += 1
             return
         
+        # 현재 행에 가능한 열 : (N 크기의 열)  & ~(이전에 queen 놓은 열, 왼/오 대각선)
+        # 즉, N 크기만큼 가능한 열에서 이전에 놓은 열, 왼/오 대각선에 퀸을 놓은 열은 제외
         available_cols = ((1 << N) - 1) & ~(cols | left_diag | right_diag)
         
         while available_cols:
-            pos = available_cols & -available_cols
-            available_cols -= pos
+            pos = available_cols & -available_cols # 가장 오른쪽에 있는 1 찾기
+            available_cols -= pos # 찾은 위치에 해당하는 열을 사용하지 않도록 표시 (pos 위치에 0)
+            # -> 가능한 열에서 퀸을 배치할 때마다 해당 열을 사용하지 않도록 표시해줌
             
             n_queen(row + 1, cols | pos, (left_diag | pos) << 1, (right_diag | pos) >> 1)
+            # cols | pos : pos 열에 퀸 배치
+            # (left_diag | pos)<<1 : 왼쪽 대각선 저장에 퀸 배치 체크. 그리고 row+1이니까 얘도 한 칸 옆으로
+            
             
     answer = 0
     n_queen(0, 0, 0, 0)
--- 프로그래머스/2/12952. N－Queen/README.md
+++ 프로그래머스/2/12952. N－Queen/README.md
@@ -1,10 +1,10 @@
 # [level 2] N-Queen - 12952 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12952) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12952#) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 485.76 ms
+메모리: 10.2 MB, 시간: 599.63 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 09일 18:57:49
+2024년 05월 09일 20:58:46
 
 ### 문제 설명
 
